451f719caf0796d842742762ba507bab81c65607
mherren2 <m@maeffew.com>
2022-01-07 09:05:18 +0100

    Initial commit

---
+++ _nuxt/static/1624999714/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Finfos","404","\u002Fservices","\u002F404"]})
---
+++ js/ieonly.js
@@ -0,0 +1,6 @@
+if(/Trident/.test(window.navigator.userAgent)){
+    //console.log('is IE', window.navigator.userAgent);
+    alert("This site uses technologies not compatible with Internet Explorer 8, 9, 10 and 11. \nPlease use Edge which is the successor to Internet Explorer or any other browser.")
+}
+
+//# sourceURL=ieonly
